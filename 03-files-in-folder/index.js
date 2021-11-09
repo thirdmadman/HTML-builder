b960945ffde9fe498fa0f305c71117f662be6f9d
thirdmadman <thirdmadman@gmail.com>
2021-11-09 17:46:50 +0400

    fix: round size to 2 digits

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,7 +7,7 @@ async function getFilesInfoInPath(folderPath) {
     for (let item of items) {
       const fileInfo = await fs.promises.stat(resolve(folderPath, item));
       if (fileInfo.isFile()) {
-        console.log(`${parse(item).name} - ${parse(item).ext.slice(1)} - ${fileInfo.size > 1024 ? fileInfo.size / 1024 + 'kb' : fileInfo.size + 'b'}`);
+        console.log(`${parse(item).name} - ${parse(item).ext.slice(1)} - ${fileInfo.size > 1024 ? (fileInfo.size / 1024).toFixed(2) + 'kb' : fileInfo.size + 'b'}`);
       }
     }
   }
