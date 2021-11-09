cc8a5cbd26040ba98fa3a6bf408c691e2b0e2f1a
thirdmadman <thirdmadman@gmail.com>
2021-11-09 16:29:38 +0400

    feat: impl getFilesInfoInPath

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const { resolve, parse } = require('path');
+
+async function getFilesInfoInPath(folderPath) {
+  const items = await fs.promises.readdir(folderPath);
+  if (items.length) {
+    for (let item of items) {
+      const fileInfo = await fs.promises.stat(resolve(folderPath, item));
+      if (fileInfo.isFile()) {
+        console.log(`${parse(item).name} - ${parse(item).ext.slice(1)} - ${fileInfo.size > 1024 ? fileInfo.size / 1024 + 'kb' : fileInfo.size + 'b'}`);
+      }
+    }
+  }
+}
+
+getFilesInfoInPath(resolve(__dirname, './secret-folder'));
